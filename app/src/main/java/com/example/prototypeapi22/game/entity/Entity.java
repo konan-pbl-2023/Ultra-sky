93a0a48396cec301259b25b39108c35189319623
shouhei-yamagiwa <147803773+shouhei-yamagiwa@users.noreply.github.com>
2023-11-16 15:51:39 +0900

    ・ファイル名の変更

---
+++ app/src/main/java/com/example/prototypeapi22/game/entity/Entity.java
@@ -0,0 +1,54 @@
+package com.example.prototypeapi22.game.entity;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+
+public class Entity {
+
+    private Bitmap texture;
+
+    private int posX;
+    private int posY;
+
+    private int width;
+    private int height;
+
+    public Entity(Bitmap texture, int posX, int posY, int width, int height) {
+        this.texture = texture;
+        this.posX = posX;
+        this.posY = posY;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void draw(Canvas c) {
+        int texWidth = texture.getWidth();
+        int texHeight = texture.getHeight();
+        Rect src = new Rect(0, 0, texWidth, texHeight);
+
+        int top = posY - (height / 2);
+        int left = posX - (width / 2);
+        int right = posX + (width / 2);
+        int bottom = posY + (height / 2);
+        Rect dest = new Rect(left, top, right, bottom);
+
+        c.drawBitmap(texture, src, dest, null);
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
