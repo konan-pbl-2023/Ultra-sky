34d8bc3c902a3d5fbd1de92899bce447488f58f3
shouhei-yamagiwa <147803773+shouhei-yamagiwa@users.noreply.github.com>
2023-11-17 11:38:54 +0900

    ゲームエフェクトの追加

---
+++ app/src/main/java/com/example/prototypeapi22/game/effect/effects/EffectSpeedUp.java
@@ -0,0 +1,10 @@
+package com.example.prototypeapi22.game.effect.effects;
+
+import com.example.prototypeapi22.game.effect.Effect;
+
+public class EffectSpeedUp extends Effect {
+
+    public EffectSpeedUp() {
+        super("Speed Up", 5000);
+    }
+}
