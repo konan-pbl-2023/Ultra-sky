5a371807e6435b98793f06ee9f3fc0c1f23614b6
shouhei-yamagiwa <147803773+shouhei-yamagiwa@users.noreply.github.com>
2023-11-17 15:26:19 +0900

    警告の解決

--- app/src/main/java/com/example/prototypeapi22/game/entity/EntityPlayer.java
+++ app/src/main/java/com/example/prototypeapi22/game/entity/EntityPlayer.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 public class EntityPlayer extends Entity {
 
-    private ArrayList<Effect> effects = new ArrayList<>();
+    private final ArrayList<Effect> effects = new ArrayList<>();
 
     private boolean collideTop = false;
     private boolean collideBottom = false;
